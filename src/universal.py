5357f8d28e902b7a5d3c62fdfcaca97f0fa4caba
l-c-mcc <liam.mccracken@me.com>
2023-05-02 20:21:28 -0700

    created library module to be used across project

---
+++ src/universal.py
@@ -0,0 +1,22 @@
+# this file contains all types, data, and helper functions used across the game
+
+###############
+# Data
+###############
+
+def prepareSprite():
+    return 1
+
+SPRITES = {
+    "board" : prepareSprite()
+}
+
+###############
+# Types
+###############
+
+Coord2D = tuple[int, int]
+
+###############
+# helpers
+###############
